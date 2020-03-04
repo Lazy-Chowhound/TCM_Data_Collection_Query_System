434672300106ec013f8959ab1e655873dc364b83
Lazy-Chowhound <1103036472@qq.com>
2020-03-04 13:34:56 +0800

    添加百度词库转txt方法

--- Apriori算法/numbering.py
+++ Apriori算法/numbering.py
@@ -1,11 +1,10 @@
-import jieba
 import jieba.analyse
 import re
 
 
 def remove_useless(string):
     """
-    去除括号和一些停用词
+    简单地去除括号及其内容以及用量
     :param string:
     :return:
     """
@@ -16,12 +15,28 @@ def remove_useless(string):
     return s
 
 
+def remove_stopwords(words):
+    """
+    去除停用词
+    :param words:
+    :return:
+    """
+    stop_words = ['片', ' ', ',']
+    count = 0
+    while count < len(words):
+        if words[count] in stop_words:
+            words.pop(count)
+        else:
+            count += 1
+    return words
+
+
 if __name__ == '__main__':
-    dict = {'A': 1}
     s = "白术3克（炒）,云苓3克 蔻米1.5克（研）法夏3克 扁豆9克（炒）制草1.5克 煨姜1片 伏龙肝3克"
     s = remove_useless(s)
 
-    jieba.load_userdict('medical.txt')
-    jieba.analyse.set_stop_words('stop_words.txt')
+    jieba.load_userdict('中药材词库.txt')
     words = jieba.cut(s, cut_all=False, HMM=True)
-    print("/".join(words))
+    words = list(words)
+
+    print(remove_stopwords(words))
