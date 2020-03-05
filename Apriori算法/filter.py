dd91b2e2e93eac48dba514406e53b32539f7b36e
Lazy-Chowhound <1103036472@qq.com>
2020-03-05 15:41:00 +0800

    对提取的药材二次过滤

--- Apriori算法/filter.py
+++ Apriori算法/filter.py
@@ -4,7 +4,7 @@ import mysql.connector
 class Filter:
     connection = None
     cursor = None
-    filt_words = ['、', ]
+    filt_words = ['各', '半', '钱', '适量', '分', '少', '许', '斤', '两']
 
     def __init__(self):
         try:
@@ -17,12 +17,43 @@ class Filter:
         self.cursor.execute('SELECT * FROM prescription')
         count = 1
         for item in self.cursor.fetchall():
-            drug = item[6]
+            drug = str(item[6])
+            name = str(item[0])
+            if '半两' in drug:
+                drug = drug.replace("半两", "")
+            if '半钱' in drug:
+                drug = drug.replace("半钱", "")
+            if '、' in drug:
+                drug = drug.replace('、', '/')
+            if '/钱半' in drug:
+                drug = drug.replace('/钱半', '')
             if '各' in drug:
-                print(count)
+                drug = drug.replace('各', '')
+            if '//' in drug:
+                drug = drug.replace('//', '/')
+            for word in self.filt_words:
+                if word in drug:
+                    print(count)
+                    break
+            self.cursor.execute("UPDATE prescription SET drug=%s WHERE name=%s", [drug, name, ])
+            self.connection.commit()
+            count += 1
+
+    def is_split(self):
+        self.cursor.execute('SELECT * FROM prescription')
+        count = 1
+        for item in self.cursor.fetchall():
+            drug = str(item[6])
+            # 判断是否有未分割的药材
+            split_drug = drug.split("/")
+            for each in split_drug:
+                if len(each) == 4:
+                    print(count)
+                    break
             count += 1
 
 
 if __name__ == '__main__':
     filter = Filter()
     filter.filt()
+    filter.is_split()
