9e2082bc6f380ff6fd2ad17686509c474debdf58
Lazy-Chowhound <1103036472@qq.com>
2020-03-23 14:31:00 +0800

    重构代码 完善功能

--- PCA降维/pca.py
+++ PCA降维/pca.py
@@ -12,6 +12,7 @@ class Cluster:
     standardX = None
     newX = None
     kMeansModel = None
+    category = 0
 
     def readData(self):
         source_file = open("herbData.csv", "r", encoding="utf-8-sig")
@@ -30,40 +31,58 @@ class Cluster:
         scaler = StandardScaler()
         self.standardX = scaler.fit_transform(self.X)
 
-    def reduceDimension(self):
+    def reduceDimension(self, dimension):
         """
         PCA降维
         :return:
         """
-        pca = PCA(n_components=2)
+        pca = PCA(n_components=dimension)
         self.newX = pca.fit_transform(self.standardX)
-        print(pca.explained_variance_ratio_)
-        # count = 0
-        # for each in pca.explained_variance_ratio_:
-        #     count += each
-        # print(count)
+        print("降为{}维后各维度占比{}".format(dimension, pca.explained_variance_ratio_))
 
-    def kMeans(self, kind):
-        self.kMeansModel = KMeans(n_clusters=kind, max_iter=300).fit(self.newX)
+    def kMeans(self, category):
+        """
+        KMeans聚类
+        :param category:
+        :return:
+        """
+        self.category = category
+        self.kMeansModel = KMeans(n_clusters=category, max_iter=300).fit(self.newX)
         score = calinski_harabasz_score(self.newX, self.kMeansModel.labels_)
         # 评价标准之一calinski_harabasz分数 越大越好
-        print("聚类{}簇的calinski_harabasz分数为".format(str(score)))
+        print("聚类{}簇的calinski_harabasz分数为{}".format(category, str(score)))
 
-    def drawGraph(self, kind):
-        plt.figure(figsize=(10, 10))
-        colors = ['g', 'r', 'y', 'b', 'darkorange', 'indigo', 'cyan', 'peru', 'darkcyan', 'darkmagenta', 'gray',
-                  'yellow']
-        markers = ['o', 's', 'd', 'h']
+    def clusterResult(self):
+        """
+        聚类结果
+        :return:
+        """
         statistics = {}
-        for i in range(kind):
+        for i in range(self.category):
             statistics[i] = 0
-        for i, l in enumerate(self.kMeansModel.labels_):
-            plt.plot(self.newX[i][0], self.newX[i][1], color=colors[l], marker=markers[1], ls='None')
-            statistics[l] += 1
+        for each in self.kMeansModel.labels_:
+            statistics[each] += 1
         print(statistics)
-        # 簇中心点
 
-        plt.title("Medicine Categories")
+    def drawGraph(self):
+        """
+        维度为2时可视化
+        :return:
+        """
+        plt.figure(figsize=(10, 10))
+        colors = ['grey', 'red', 'peru', 'gold', 'olivedrab', 'steelblue', 'slateblue', 'blueviolet']
+        markers = ['o', 's', 'd', 'h', 'p', '^', 'v', '.']
+
+        for i, l in enumerate(self.kMeansModel.labels_):
+            plt.plot(self.newX[i][0], self.newX[i][1], color=colors[l], marker=markers[l], ls='None')
+
+        # 每个簇类的中心点
+        count = 0
+        for point in self.kMeansModel.cluster_centers_:
+            plt.plot(point[0], point[1], color=colors[count], marker='x', ls='None', markersize=12)
+            count += 1
+
+        plt.title("kMeans")
         plt.show()
 
 
@@ -71,5 +90,7 @@ if __name__ == '__main__':
     cluster = Cluster()
     cluster.readData()
     cluster.dataStandardization()
-    cluster.reduceDimension()
-    cluster.drawGraph(12)
+    cluster.reduceDimension(2)
+    cluster.kMeans(7)
+    cluster.clusterResult()
+    cluster.drawGraph()
